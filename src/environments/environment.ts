0cde2b4b6afb80b1c817aa13dea865c960cc22fc
Abdulrahman Khaled Adam <abdulrahmankhadamm@gmail.com>
2025-10-11 19:20:56 +0300

    Edited APIs base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://myprotofoliobackend-production.up.railway.app/api',
   appName: 'abdallah hany Portfolio',
   version: '1.0.0',
   features: {
     auth: true,
     admin: true,
     projects: true,
-    skills: true
-  }
+    skills: true,
+  },
 };
